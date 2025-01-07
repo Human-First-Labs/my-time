92dbcc6295a25b38cf86928c58ae3fd7ec114461
Maurovic <maurovic.cachia@gmail.com>
2025-01-07 19:38:35 +0100

    Svelte learning

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import Header from '$lib/Header.svelte';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<meta name="description" content="Svelte demo app" />
+	<link rel="stylesheet" href="https://unpkg.com/carbon-components-svelte@0.30.0/css/g10.css" />
+</svelte:head>
+
+
+<div class="app">
+	<Header />
+
+	<main>
+		{@render children()}
+	</main>
+
+	<footer>
+		<p>
+			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+		</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+	import { SkeletonText } from 'carbon-components-svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<SkeletonText paragraph>
+		This is a test
+	</SkeletonText>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+</style>
